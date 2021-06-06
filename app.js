bbdd5970d75009c47705865ef0cd6b12300dbe70
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2021-06-06 15:37:39 -0700

    general update

--- app.js
+++ app.js
@@ -7,17 +7,21 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.use('/', (req, res, next) => {
-    console.log(data);
-    next();
+    res.send('INDEX'); 
 });
 
 app.use('/about', (req, res, next) => {
-
+    res.send('ABOUT');
 });
 
 app.use('/projects/:id', (req, res, next) => {
 
 });
+
+app.use((req, res, next) => {
+    const err = new Error('Page Not Found');
+    res.send('<h1>Page Not Found</h1>');
+})
 
 app.listen(3000, () => {
     console.log("App is running at localhost:3000")
